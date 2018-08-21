a574e3a3acbdce9fccbde1f40af0b70f349f18aa
Aaron Bullard <aaron@2book.com>
2018-08-20 22:33:17 -0400

    refactored Factory

--- src/Factory.php
+++ src/Factory.php
@@ -3,8 +3,7 @@
 namespace PhpSchema;
 
 use ArrayObject;
-use PhpSchema\Models\Model;
-use PhpSchema\Traits\PublicProperties;
+use PhpSchema\Models\SchemaModel;
 
 class Factory
 {
@@ -18,53 +17,14 @@ class Factory
      */
     public static function createDTO($schema, array $args = [])
     {
-        return new class($args, $schema) extends Model
+        return new class($args, $schema) extends SchemaModel
         {
-            // use PublicProperties;
-
             protected static $schema;
-
-            protected $_validator;
 
             public function __construct($args, $schema)
             {
                 static::$schema = $schema;
-                $this->_validator = new Validator;
-
-                parent::__construct($args, ArrayObject::ARRAY_AS_PROPS);
-
-                foreach($this as $offset => $value){
-                    $this->stopObserving($offset);
-                    $this->startObserving($offset);
-                }
-
-                $this->notify();
-            }
-
-            public function getSchema()
-            {
-                return static::$schema;
-            }
-
-            public function notify($payload = null): void
-            {
-                $this->validate();
-
-                \array_walk($this->subscribers, function($sub) use ($payload){
-                    $sub->notify($payload);
-                });
-            }
-
-            public function validate(): void
-            {
-                $object = $this->toObject();
-
-                $this->_validator->validate($object, $this->getSchema());
-
-                if($this->_validator->isValid() == false){
-                    $errors = $this->_validator->getErrors();
-                    throw ValidationException::withErrors($errors);
-                }
+                parent::__construct($args);
             }
         };
     }
